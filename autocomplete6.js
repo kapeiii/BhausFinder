1224951815f71f202e2fda87390bd4681562aee5
kapeiii <138109641+kapeiii@users.noreply.github.com>
2023-10-06 06:19:51 +0800

    Add files via upload

---
+++ autocomplete6.js
@@ -0,0 +1,39 @@
+let availablKeywords = [
+    'Moonlight Mansion',
+    'Victoria Charm Inn',
+    'Coastal Comfort Lodge',
+    'Starry Night Haven',
+    'Mang Tomas BH',
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+        result = availablKeywords.filter((keyword)=>{
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+    display(result);
+
+    if(!result.length){
+        resultsBox.innerHTML = '';
+    }
+}
+
+function display(result){
+    const content = result.map((list)=>{
+        return "<li onclick = selectInput(this)>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
+}
+
+function selectInput(list){
+    inputBox.value = list.innerHTML;
+    resultsBox.innerHTML = '';
+}
